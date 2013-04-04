059e589a66a543b4b07f2b205658826fb410c450
akima <akm2000@gmail.com>
2013-04-04 17:42:33 +0900

    Initial Commit

---
+++ cocos2dx-ios/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  cocos2dx_iosAppController.h
+//  cocos2dx-ios
+//
+//  Created by  on 13/04/04.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ cocos2dx-ios/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  cocos2dx_iosAppController.h
+//  cocos2dx-ios
+//
+//  Created by  on 13/04/04.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
