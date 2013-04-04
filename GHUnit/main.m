ba22c31a7eaef8212e1871fe15c29d3332166880
akima <akm2000@gmail.com>
2013-04-04 18:47:43 +0900

    setup GHUnit. [IMPORTANT] -ObjC option is required, but -all_load option must not be included

---
+++ GHUnit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GHUnit
+//
+//  Created by  on 13/04/04.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GHUnitIOS/GHUnit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
